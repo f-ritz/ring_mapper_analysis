17c64f154e79bd90466cdf818761146f86ce70dd
Joe Yesselman <jyesselm@Joes-MacBook-Air.local>
2022-10-26 15:55:44 -0500

    added histogram functionality for ring mapper (based on mutations in i, 1D)

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <istream>
 #include <ostream>
 #include <stdexcept>
+#include <numeric>
 
 int main(int argc, char **argv) {
     std::cout << "Processing..." << std::endl;
@@ -21,6 +22,7 @@ int main(int argc, char **argv) {
     int counter = 0;
     int vector_size = 0;
     int num_mutations_int = 0;
+    int sum_muts = 0;
     String filename(argv[1]);
     String line;
     String data;
@@ -46,7 +48,28 @@ int main(int argc, char **argv) {
     vector_size = data_counter_single.length();
     std::ofstream ring_mapper_analysis_csv;
     ring_mapper_analysis_csv.open("ring_mapper_results.csv");
-    ring_mapper_analysis_csv << "i" << "," << "j" << "," << "chisq" << "," << "a" << "," << "b" << "," << "c" << "," << "d" << std::endl;
+    ring_mapper_analysis_csv << "i" << "," << "j" << "," << "chisq" << "," << "a" << "," << "b" << "," << "c" << ","
+                             << "d" << std::endl;
+    std::ofstream ring_mapper_histogram_csv;
+    ring_mapper_histogram_csv.open("ring_mapper_histogram.csv");
+    //std::ofstream ring_mapper_histogram_positions_csv;
+    //ring_mapper_histogram_positions_csv.open("ring_mapper_positions.csv");
+    std::ofstream debug_output_csv;
+    debug_output_csv.open("debug_output.csv");
+    /*
+    std::ofstream ring_mapper_a_csv;
+    ring_mapper_a_csv.open("ring_mapper_a.csv");
+    ring_mapper_a_csv << "i,j" << std::endl;
+    std::ofstream ring_mapper_b_csv;
+    ring_mapper_b_csv.open("ring_mapper_b.csv");
+    ring_mapper_b_csv << "i,j" << std::endl;
+    std::ofstream ring_mapper_c_csv;
+    ring_mapper_c_csv.open("ring_mapper_c.csv");
+    ring_mapper_c_csv << "i,j" << std::endl;
+    std::ofstream ring_mapper_d_csv;
+    ring_mapper_d_csv.open("ring_mapper_d.csv");
+    ring_mapper_d_csv << "i,j" << std::endl;
+     */
     std::vector<std::vector<float>> a;
     std::vector<std::vector<float>> b;
     std::vector<std::vector<float>> c;
@@ -62,6 +85,13 @@ int main(int argc, char **argv) {
         d.push_back(v);
     }
     double five_percent_cutoff = 0.05 * vector_size;
+    std::vector<float> mut_positions;
+    std::vector<float> mut_positions_frequency;
+    for (int i = 0; i < vector_size; i++) {
+        mut_positions.push_back(0.0f);
+        mut_positions_frequency.push_back(0);
+    }
+
     while (input_file.good()) {
         getline(input_file, line);
         if (line.length() < 2) {
@@ -70,8 +100,7 @@ int main(int argc, char **argv) {
         data_temp = split(line, "\t");
         if (counter > 2) /* trims the 1st 3 lines */ {
             data = data_temp[1];
-            num_mutations_str = data_temp[2];
-            num_mutations_int = std::stoi(num_mutations_str);
+            num_mutations_int = std::stoi(data_temp[2]);
             n += 1;
             if (n > 1000) {
                 break;
@@ -91,6 +120,9 @@ int main(int argc, char **argv) {
                             if (mut_i == 0 && mut_j == 0) {
                                 a[i][j]++;
                             } else if (mut_i == 1 && mut_j == 0) {
+                                if (num_mutations_int == 1) {
+                                    mut_positions[i]++;
+                                }
                                 b[i][j]++;
                             } else if (mut_i == 0) {
                                 c[i][j]++;
@@ -104,6 +136,17 @@ int main(int argc, char **argv) {
         }
         counter++;
     }
+
+    sum_muts = std::accumulate(mut_positions.begin(), mut_positions.end(), 0);
+    ring_mapper_histogram_csv << "i,histogram_values" << std::endl;
+    //ring_mapper_histogram_positions_csv << "i, num_single_muts, total_muts: " << sum_muts << std::endl;
+    for (int i = 0; i < vector_size; i++) {
+        mut_positions_frequency[i] = (mut_positions[i] / sum_muts);
+        //ring_mapper_histogram_positions_csv << i + 1 << "," << mut_positions[i] << std::endl;
+        ring_mapper_histogram_csv << i + 1 << "," << mut_positions_frequency[i] << std::endl;
+    }
+    //std::cout << std::accumulate(mut_positions_frequency.begin(), mut_positions_frequency.end(), 0) << std::endl;
+
     for (int i = 0; i < vector_size; i++) {
         for (int j = i + 1; j < vector_size; j++) {
             float dim = (a[i][j] + b[i][j]) * (c[i][j] + d[i][j]) * (a[i][j] + c[i][j]) * (b[i][j] + d[i][j]);
@@ -115,7 +158,8 @@ int main(int argc, char **argv) {
                 continue;
             }
             if (chisq > 20) {
-                ring_mapper_analysis_csv << i + 1 << "," << j + 1 << "," << chisq << "," << a[i][j] << "," << b[i][j] << "," << c[i][j] << "," << d[i][j] << std::endl;
+                ring_mapper_analysis_csv << i + 1 << "," << j + 1 << "," << chisq << "," << a[i][j] << "," << b[i][j]
+                                         << "," << c[i][j] << "," << d[i][j] << std::endl;
             }
         }
     }
